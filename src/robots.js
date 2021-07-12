117bc3f53f0f779411684af56ab95cc0aec03fa6
vaishali <vaishali@Ashoks-Air.attlocal.net>
2021-07-12 16:04:44 -0500

    Adding files

---
+++ src/robots.js
@@ -0,0 +1,47 @@
+export const robots = [
+{
+	id:101,
+	name:'Ashok Pehere',
+	email: 'pehere@gmail.com',
+},
+{
+	id:102,
+	name:'Vaishali Pehere',
+	email: 'vpehere@gmail.com',
+},
+{
+	id:103,
+	name:'Atharv Pehere',
+	email: 'aapehere@gmail.com',
+},
+{
+	id:104,
+	name:'Kunal Pehere',
+	email: 'kdpehere@gmail.com',
+},
+{
+	id:105,
+	name:'Kalpesh Pehere',
+	email: 'kad@gmail.com',
+},
+{
+	id:106,
+	name:'Dyaneshwar Pehere',
+	email: 'dd@gmail.com',
+},
+{
+	id:107,
+	name:'Dattatray Pehere',
+	email: 'd@gmail.com',
+},
+{
+	id:108,
+	name:'Sakharbai Pehere',
+	email: 'sd@gmail.com',
+},
+{
+	id:109,
+	name:'Sunita Pehere',
+	email: 'spehere@gmail.com',
+}
+]
